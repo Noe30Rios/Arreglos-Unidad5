5f644dafbd2dc683fb7b2cbb14f6522a346cba10
Noe30Rios <86136114+Noe30Rios@users.noreply.github.com>
2021-06-23 22:17:26 -0600

    Ejercicio 2
    Aplicación 2 de 6

---
+++ cadena/cadena/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace cadena
+{
+    class Program
+    {
+        class cadena
+        {
+            string CadenaS;
+            public void nombre()
+            {
+                Console.WriteLine("introduce una palabra :");
+                CadenaS = Convert.ToString(Console.ReadLine());
+                char[] cArray = CadenaS.ToCharArray();
+                string Reverse = String.Empty;
+                for (int i = cArray.Length - 1; i > -1; i--)
+                {
+                    Reverse += cArray[i];
+                }
+                Console.WriteLine("la palabra alreves es :" + Reverse);
+                Console.ReadKey();
+            }
+
+            public void Mayus()
+            {
+                Console.WriteLine("la cadena en mayusculas es: {0}", CadenaS.ToUpper());
+                Console.ReadKey();
+                Console.WriteLine("la cadena Nueva es: {0}", CadenaS.Replace("a", "4").Replace("i", "1").Replace("s", "5").Replace("b", "8").Replace("e", "3").Replace("t", "7"));
+                Console.ReadKey();
+            }
+          
+           
+        }
+        static void Main(string[] args)
+        {
+            cadena objcade = new cadena();
+            objcade.nombre();
+            cadena objcadesa = new cadena();
+            objcade.Mayus();
+            
+
+        }
+    }
+}
